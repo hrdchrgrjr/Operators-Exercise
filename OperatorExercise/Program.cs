7218518adf4a95da12125faf3ac6f5c74aad0ba5
hrdchrgrjr <hrdchrgrjr@gmail.com>
2023-06-19 14:15:02 -0400

    Exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -12,6 +12,18 @@
             {
                 Console.WriteLine(a + "/" + b + " is " + quotient + " remainder " + remainder);
             }
+
+            Console.WriteLine("Enter the radius of a circle");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
+
 }
